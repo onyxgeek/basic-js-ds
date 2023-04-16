4d361fb39c44f70836b0aec4aadb68e1e077ccc4
onyxgeek <onyxpeek@gmail.com>
2023-04-16 23:26:52 +0300

    feat: add first implementation for the code with only one error:)

--- src/queue.js
+++ src/queue.js
@@ -14,20 +14,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  constructor(){
+    this.elements = [];
+  }
 
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue( value) {
+    this.elements.push(value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.elements.length === 0) {
+      return null;
+    }
+    return this.elements.pop();
   }
 }
 
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+  getUnderlyingList() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
