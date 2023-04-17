f0f54ad5cb4818776495e324bf909b05e1f7b87f
onyxgeek <onyxpeek@gmail.com>
2023-04-17 13:33:13 +0300

    feat: finish the remove-from-the-list.js

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,10 +22,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  // If the linked list is empty, return null
+  if (!l) {
+    return null;
+  }
+
+  // Traverse the linked list
+  let current = l;
+  let previous = null;
+  while (current) {
+    // If the current node's value equals k, skip it
+    if (current.value === k) {
+      if (previous) {
+        previous.next = current.next;
+      } else {
+        l = current.next;
+      }
+    } else {
+      previous = current;
+    }
+    current = current.next;
+  }
+
+  return l;
 }
+
 
 
 module.exports = {
--- src/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
